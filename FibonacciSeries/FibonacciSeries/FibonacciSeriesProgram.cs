e0de6c3ed110278d09ba49f21b4df7a035065737
iam100rub <70850027+iam100rub@users.noreply.github.com>
2021-05-06 11:36:27 +0530

    Fibonacci Series
    Fibonacci Series

---
+++ FibonacciSeries/FibonacciSeries/FibonacciSeriesProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    public class FibonacciSeriesProgram
+    {
+        static void Main(string[] args)
+        {
+            int length = 8;
+            Fibonacci(0, 1, 1, length);
+            Console.ReadKey();
+        }
+        //print the fibonacci series for a given length
+        public static void Fibonacci(int firstnumber, int secondnumber, int count, int length)
+        {
+            if (count <= length)
+            {
+                Console.Write("{0} ", firstnumber);
+                Fibonacci(secondnumber, firstnumber + secondnumber, count + 1, length);
+            }
+        }
+    }
+}
---
+++ FibonacciSeries/UnitTestProject1/FibonacciSeriesTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FibonacciSeries;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciSeriesUnitTestProject
+{
+    [TestClass]
+    public class FibonacciSeriesTest
+    {
+        /*
+         To run the unit test case follow below steps:
+         1. Click on test tab of visual studio.
+         2. Click on Run from test dropdown
+         3. Click on All Test or the kind of test case we need to run
+         4. The results will be shown on output window, also we can see the result of test cases 
+         on test explorer window
+             */
+        [TestMethod]
+        public void FibonacciTest()
+        {
+            int length = 10;
+            FibonacciSeriesProgram.Fibonacci(0, 1, 1, length);
+        }
+    }
+}
